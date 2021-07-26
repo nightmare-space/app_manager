087fec176723ad2eb9de35b1128f541a672ecc6f
Nightmare-MY <mengyanshou@gmail.com>
2021-07-26 19:45:11 +0800

    commit code

--- android/app/src/main/java/com/nightmare/appmanager/AppInfo.java
+++ android/app/src/main/java/com/nightmare/appmanager/AppInfo.java
@@ -3,100 +3,185 @@ package com.nightmare.appmanager;
 /**
  * Created by xdj on 2017/3/7.
  */
+
+import android.annotation.SuppressLint;
 import android.content.*;
 import android.content.pm.*;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.*;
+import android.net.LocalServerSocket;
+import android.net.LocalSocket;
 import android.os.Build;
+import android.os.Looper;
 import android.util.Log;
 
-import androidx.core.content.ContextCompat;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
 
-public class AppInfo
- {
+public class AppInfo {
     Context context;
     PackageManager pm;
+    private static final int MESSAGE_MAX_SIZE = 1 << 18; // 256k
 
     public AppInfo(Context context) {
         this.context = context;
         pm = context.getPackageManager();
     }
 
-    /*
-     * 获取程序 图标
-     */
-    public Drawable getAppIcon(String packname) {
-        try {
-            ApplicationInfo info = pm.getApplicationInfo(packname, 0);
-
-            return info.loadIcon(pm);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-
-        }
-        return null;
+    private static Socket connect() throws IOException {
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress("localhost", 4041));
+        return socket;
     }
 
-     public  synchronized Bitmap getBitmap(String packname) {
-         ApplicationInfo applicationInfo = null;
-         try {
-             applicationInfo = pm.getApplicationInfo(
-                     packname, 0);
-         } catch (PackageManager.NameNotFoundException e) {
-             e.printStackTrace();
-         }
-         Log.d("debug",packname);
-         assert applicationInfo != null;
-         Drawable icon = applicationInfo.loadIcon(pm); //xxx根据自己的情况获取drawable
-         try {
-             if (icon == null) {
-                 return null;
-             }
-             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && icon instanceof AdaptiveIconDrawable) {
-                 Bitmap bitmap = Bitmap.createBitmap(icon.getIntrinsicWidth(), icon.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-                 Canvas canvas = new Canvas(bitmap);
-                 icon.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-                 icon.draw(canvas);
-                 return bitmap;
-             } else {
-                 return ((BitmapDrawable) icon).getBitmap();
-             }
-         } catch (Exception e) {
-             return null;
-         }
-     }
-
-
-    /*
-     *获取程序的版本号
-     */
-    public String getAppVersion(String packname) {
-
-        try {
-            PackageInfo packinfo = pm.getPackageInfo(packname, 0);
-            return packinfo.versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-
-        }
-        return null;
+    static public byte[] Bitmap2Bytes(Bitmap bm) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        return baos.toByteArray();
     }
 
+    public static void main(String[] arg) throws Exception {
+        Looper.prepareMainLooper();
+        Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+        Constructor<?> activityThreadConstructor = activityThreadClass.getDeclaredConstructor();
+        activityThreadConstructor.setAccessible(true);
+        Object activityThread = activityThreadConstructor.newInstance();
+        Method getSystemContextMethod = activityThreadClass.getDeclaredMethod("getSystemContext");
+        Context ctx = (Context) getSystemContextMethod.invoke(activityThread);
 
-    /*
-     * 获取程序的名字
-     */
-    public String getAppName(String packname) {
+        System.out.println("执行中");
+        System.out.println(ctx);
+        new Thread(() -> {
+            try {
+                System.out.println("startIconServer");
+                AppInfo.startIconServer(ctx);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                System.out.println("startAppInfoServer");
+                AppInfo.startAppInfoServer(ctx);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        System.out.println("wait");
+        System.in.read();
+
+    }
+
+    public static void startIconServer(Context context) throws IOException {
+        @SuppressLint("PrivateApi")
+        ServerSocket serverSocket = new ServerSocket(4041);
+        while (true) {
+            Log.e("socket启动", "socket启动");
+            Socket socket = serverSocket.accept();
+
+            InputStream is = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String packageName = br.readLine();
+            OutputStream os = socket.getOutputStream();
+            AppInfo appInfo = new AppInfo(context);
+            Bitmap bitmap = appInfo.getBitmap(packageName);
+            if (bitmap != null) {
+                os.write(Bitmap2Bytes(bitmap));
+            }
+            socket.close();
+        }
+    }
+
+    private static List<String> stringToList(String strs) {
+        String[] str = strs.split(" ");
+        return Arrays.asList(str);
+    }
+
+    public static void startAppInfoServer(Context context) throws IOException {
+        @SuppressLint("PrivateApi")
+        ServerSocket serverSocket = new ServerSocket(4042);
+        while (true) {
+//        Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+//        Constructor<?> activityThreadConstructor = activityThreadClass.getDeclaredConstructor();
+//        activityThreadConstructor.setAccessible(true);
+//        Object activityThread = activityThreadConstructor.newInstance();
+            Log.e("socket启动", "socket启动");
+            Socket socket = serverSocket.accept();
+
+            InputStream is = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            OutputStream os = socket.getOutputStream();
+            List<String> id = stringToList(br.readLine());
+            StringBuilder builder = new StringBuilder();
+            for (String packageName : id) {
+                try {
+                    PackageInfo packages = context.getPackageManager().getPackageInfo(packageName, 0);
+                    builder.append(packages.applicationInfo.loadLabel(context.getPackageManager()));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        builder.append(" ").append(packages.applicationInfo.minSdkVersion);
+                    }else{
+                        builder.append(" ").append("null");
+                    }
+                    builder.append(" ").append(packages.applicationInfo.targetSdkVersion);
+                    builder.append(" ").append(packages.versionName);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        builder.append(" ").append(packages.getLongVersionCode()).append("\n");
+                    } else {
+                        builder.append(" ").append(packages.versionCode).append("\n");
+                    }
+
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            os.write(builder.toString().getBytes());
+            socket.close();
+        }
+    }
+
+    public synchronized Bitmap getBitmap(String packname) {
+        ApplicationInfo applicationInfo = null;
         try {
-            ApplicationInfo info = pm.getApplicationInfo(packname, 0);
-            return info.loadLabel(pm).toString();
+            applicationInfo = pm.getApplicationInfo(
+                    packname, 0);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
-
         }
-        return null;
+        if (applicationInfo == null) {
+            return null;
+        }
+        Drawable icon = applicationInfo.loadIcon(pm); //xxx根据自己的情况获取drawable
+        try {
+            if (icon == null) {
+                return null;
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && icon instanceof AdaptiveIconDrawable) {
+                Bitmap bitmap = Bitmap.createBitmap(icon.getIntrinsicWidth(), icon.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                icon.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                icon.draw(canvas);
+                return bitmap;
+            } else {
+                return ((BitmapDrawable) icon).getBitmap();
+            }
+        } catch (Exception e) {
+            return null;
+        }
     }
+
 
     /*
      * 获取程序的权限
--- android/app/src/main/java/com/nightmare/appmanager/MainActivity.java
+++ android/app/src/main/java/com/nightmare/appmanager/MainActivity.java
@@ -1,5 +1,7 @@
 package com.nightmare.appmanager;
 
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -22,6 +24,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -31,6 +34,8 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
+
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
@@ -43,6 +48,20 @@ public class MainActivity extends FlutterActivity {
             App(flutterEngine);
         }).start();
 
+        new Thread(() -> {
+            try {
+                AppInfo.startIconServer(getApplicationContext());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                AppInfo.startAppInfoServer(getApplicationContext());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
 
@@ -54,30 +73,22 @@ public class MainActivity extends FlutterActivity {
                 runOnUiThread(() -> result.success(Bitmap2Bytes(bitmap)));
             }).start();
         });
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "jump").setMethodCallHandler((call, result) -> {
+            new Thread(() -> {
+                List<String> arg = stringToList(call.method);
+                Intent intent = new Intent();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                ComponentName cName = new ComponentName(arg.get(0), arg.get(1));
+                intent.setComponent(cName);
+                startActivity(intent);
+            }).start();
+        });
     }
 
     public byte[] Bitmap2Bytes(Bitmap bm) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
         return baos.toByteArray();
-    }
-
-    public void saveBitmap(Bitmap bm, String picName) {
-        File f = new File(this.getFilesDir().getPath() + "/AppManager/.icon", picName);
-        if (f.exists()) {
-            f.delete();
-        }
-        try {
-            FileOutputStream out = new FileOutputStream(f);
-            bm.compress(Bitmap.CompressFormat.PNG, 10, out);
-            out.flush();
-            out.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
     private List<String> stringToList(String strs) {
@@ -88,51 +99,47 @@ public class MainActivity extends FlutterActivity {
     void App(@NonNull FlutterEngine flutterEngine) {
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "App").setMethodCallHandler((call, result) -> new Thread(() -> {
             List<String> id = stringToList(call.method);
-            StringBuilder list = new StringBuilder();
-            for (String a : id) {
+            StringBuilder builder = new StringBuilder();
+            for (String packageName : id) {
                 try {
-                    PackageInfo packages = getPackageManager().getPackageInfo(a, 0);
-                    list.append(packages.applicationInfo.loadLabel(getPackageManager())).append("\n");
+                    PackageInfo packages = getPackageManager().getPackageInfo(packageName, 0);
+                    builder.append(packages.applicationInfo.loadLabel(getPackageManager()));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        builder.append(" ").append(packages.applicationInfo.minSdkVersion);
+                        builder.append(" ").append(packages.applicationInfo.targetSdkVersion);
+                        builder.append(" ").append(packages.versionName);
+                        builder.append(" ").append(packages.getLongVersionCode()).append("\n");
+                    }
                 } catch (PackageManager.NameNotFoundException e) {
                     e.printStackTrace();
                 }
             }
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    result.success(list.toString());
-                }
-            });
+            runOnUiThread(() -> result.success(builder.toString()));
 
         }).start());
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "GetAppInfo").setMethodCallHandler((call, result) -> new Thread(() -> {
-
             try {
                 PackageInfo packages = getPackageManager().getPackageInfo(call.method, 0);
                 ActivityInfo[] actInfo = getPackageManager().getPackageInfo(packages.packageName, PackageManager.GET_ACTIVITIES).activities;
-                StringBuilder list = new StringBuilder();
+                StringBuilder builder = new StringBuilder();
 //                if(actInfo!=null)
 //                for (ActivityInfo a : actInfo) {
 //                    list.append(a.name).append("\n");
 //                }
                 Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
                 mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+                @SuppressLint("QueryPermissionsNeeded")
                 List<ResolveInfo> appList = getPackageManager().queryIntentActivities(mainIntent, 0);
                 for (int i = 0; i < appList.size(); i++) {
                     ResolveInfo resolveInfo = appList.get(i);
                     String packageStr = resolveInfo.activityInfo.packageName;
                     if (packageStr.equals(call.method)) {
                         //这个就是你想要的那个Activity
-                        list.append(resolveInfo.activityInfo.name).append("\n");
+                        builder.append(resolveInfo.activityInfo.name).append("\n");
                         break;
                     }
                 }
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        result.success(list.toString());
-                    }
-                });
+                runOnUiThread(() -> result.success(builder.toString()));
             } catch (PackageManager.NameNotFoundException e) {
                 e.printStackTrace();
             }
